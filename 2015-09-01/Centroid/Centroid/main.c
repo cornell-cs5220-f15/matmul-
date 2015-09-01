7acc983a7683e3fbd699228723ff0eb1610f2429
Elliot Cartee <evc34@cornell.edu>
2015-09-01 18:31:55 -0400

    Initial Commit

---
+++ 2015-09-01/Centroid/Centroid/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Centroid
+//
+//  Created by Elliot Cartee on 9/1/15.
+//  Copyright (c) 2015 Elliot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
