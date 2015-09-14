937d44a6800403522c9859c89940fe9f1ccca82f
Marc Aurele Tiberius Gilles <mtg79@en-cs-totient-01.coecis.cornell.edu>
2015-09-14 14:26:10 -0400

    blocking things still

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,4 +1,6 @@
-const char* dgemm_desc = "My awesome dgemm.";
+const char* dgemm_desc = "My awesome matmul.";
+
+#include <stdlib.h>
 
 #ifndef BLOCK_SIZE
 #define BLOCK_SIZE ((int) 32)
@@ -9,23 +11,23 @@ const char* dgemm_desc = "My awesome dgemm.";
   B is K-by-N
   C is M-by-N
 
-  lda is the leading dimension of the matrix (the M of square_dgemm).
+  lda is the leading dimension of the matrix (the M of square_>=m).
 */
 
 
-void row_to_block(const int M, const int nblock, const double *A, const double *newA){
+void row_to_block(const int M, const int nblock, const double *A, double *newA){
 	// converts to block indexing and pads the new matrix with zeros so that it is divisble by BLOCK_SIZE
 
 		
-	int blocki, blockj,i,j;	
-	for(blocki=0; blocki < nblock, ++blocki){
-		for(blockj=0; blockj < nblock, ++blockj){
+	int bi, bj,i,j;	
+	for(bi=0; bi < nblock; ++bi){
+		for(bj=0; bj < nblock; ++bj){
 			for(i=0; i < BLOCK_SIZE; ++i){
 				for(j=0; j < BLOCK_SIZE; ++j){
-					if (bj*BLOCK_SIZE+j).ge. M || bi*blocksize+i .ge. M){ // we can optimize this to delete this "if". this is doing the padding 
+					if ((bj*BLOCK_SIZE+j) >= M || bi*BLOCK_SIZE+i >= M){ // we can optimize this to delete this "if". this is doing the padding 
 					newA[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]=0;}
 					else{
-					newA[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]=A[(j+bj*BLOCK_SIZE)*M+bi*BLOCK_SIZE+i)] }
+ 				newA[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]=A[(j+bj*BLOCK_SIZE)*M+bi*BLOCK_SIZE+i]; }
 				
 				}
 			}
@@ -34,18 +36,18 @@ void row_to_block(const int M, const int nblock, const double *A, const double *
 	}
 
 
-void block_to_row(const int M, const int nblock, const double *A, const double *newA){
+void block_to_row(const int M, const int nblock, double *A, const double *newA){
 
 		
-	int blocki, blockj,i,j;	
-	for(blocki=0; blocki < nblock, ++blocki){
-		for(blockj=0; blockj < nblock, ++blockj){
+	int bi, bj,i,j;	
+	for(bi=0; bi < nblock; ++bi){
+		for(bj=0; bj < nblock; ++bj){
 			for(i=0; i < BLOCK_SIZE; ++i){
 				for(j=0; j < BLOCK_SIZE; ++j){
-					if (bj*BLOCK_SIZE+j).ge. M || bi*blocksize+i .ge. M){ // we can optimize this to delete this "if". this is doing the padding 
+					if ((bj*BLOCK_SIZE+j)>= M || bi*BLOCK_SIZE+i >= M){ // we can optimize this to delete this "if". this is doing the padding 
 					}
 					else{
-					A[(j+bj*BLOCK_SIZE)*M+bi*BLOCK_SIZE+i)]=newA[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]; }
+				 A[(j+bj*BLOCK_SIZE)*M+bi*BLOCK_SIZE+i]=newA[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]; }
 				
 				}
 			}
@@ -57,22 +59,8 @@ void block_to_row(const int M, const int nblock, const double *A, const double *
 	
 	
 	
-void basic_dgemm(const int lda, const int M, const int N, const int K,
-                 const double *A, const double *B, double *C)
-{
-    int i, j, k;
-    for (i = 0; i < M; ++i) {
-        for (j = 0; j < N; ++j) {
-            double cij = C[j*lda+i];
-            for (k = 0; k < K; ++k) {
-                cij += A[k*lda+i] * B[j*lda+k];
-            }
-            C[j*lda+i] = cij;
-        }
-    }
-}
 
-void do_block(const int M,
+void do_block(const int M, const int nblock,
               const double *A, const double *B, double *C,
               const int bi, const int bj, const int bk)
 {
@@ -81,9 +69,9 @@ void do_block(const int M,
         for (j = 0; j < BLOCK_SIZE; ++j) {
             double cij = C[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)];
             for (k = 0; k < BLOCK_SIZE; ++k) {
-                cij += A[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+BLOCK_SIZE*i+k] * B[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+BLOCK_SIZE*k+j];
+                cij += A[((bj*nblock+bi))*BLOCK_SIZE*BLOCK_SIZE+BLOCK_SIZE*i+k] * B[((bj*nblock)+bi)*BLOCK_SIZE*BLOCK_SIZE+BLOCK_SIZE*k+j];
             }
-            C[j*lda+i] = cij;
+            C[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]= cij;
         }
     }
 }
@@ -95,28 +83,27 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
 		block j = column index of block
 		nblock = number of blocks
 	*/
-	int pad_size, blocki, blockj, i, j, nblock;
+	int pad_size, bi, bj, bk, i, j, nblock;
 	
-	if (M%Block_SIZE==0){
+	if (M%BLOCK_SIZE==0){
 		nblock=M/BLOCK_SIZE;
 		pad_size=M;}
-	else{ pad_size=((M/Block_SIZE)+1)*BLOCK_SIZE;
+	else{ pad_size=((M/BLOCK_SIZE)+1)*BLOCK_SIZE;
 		nblock=M/BLOCK_SIZE+1;}
 		
-	double *bA= (double*) malloc(pad_size*pad_size, sizeof(double));
-	double *bB= (double*) malloc(pad_size*pad_size, sizeof(double));
-	double *bC= (double*) malloc(pad_size*pad_size, sizeof(double));
-	// changes indexing
+	double *bA= (double*) malloc(pad_size*pad_size*sizeof(double));
+	double *bB= (double*) malloc(pad_size*pad_size*sizeof(double));
+	double *bC= (double*) malloc(pad_size*pad_size*sizeof(double));
+	// cha>= indexing
 	row_to_block(M,nblock, A, bA);
 	row_to_block(M,nblock, B, bB);
 	row_to_block(M,nblock, C, bC);
 	
 	
-    int bi, bj, bk;
     for (bi = 0; bi < nblock; ++bi) {
         for (bj = 0; bj < nblock; ++bj) {
             for (bk = 0; bk < nblock; ++bk) {
-                do_block(M, bA, bB, bC, i, j, k);
+                do_block(M, nblock, bA, bB, bC, bi, bj, bk);
             }
         }
     }
