5b42e6da3c5117302c35d2db4b1c97215085886b
Marc Aurele Tiberius Gilles <mtg79@en-cs-totient-01.coecis.cornell.edu>
2015-09-14 16:34:02 -0400

    64 tiny

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -3,7 +3,7 @@ const char* dgemm_desc = "My awesome matmul.";
 #include <stdlib.h>
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 16)
+#define BLOCK_SIZE ((int) 64)
 #endif
 /* template by David Bindel,
 written by Marc Aurele Gilles
