9c29373670a6e78dfdc0da21879af9639bf94c5b
Robert Chiodi <rmc298@cornell.edu>
2015-09-14 11:05:40 -0400

    Less work in determining indices. Maybe a little faster?

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -22,13 +22,16 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
     for(int K=0; K<n_blocks; ++K) {
       for(int I=0; I<n_rect; ++I){
         //Inner block loop
-          int j_end = ((J+1)*block_size < M? (J+1)*block_size : M);
-          for(int j=J*block_size;j< j_end; ++j) {
-            int k_end = ((K+1)*block_size < M? (K+1)*block_size : M);
-            for(int k=K*block_size; k<k_end; ++k) {
-              int i_end = ((I+1)*rect_length < M? (I+1)*rect_length : M);
-              for(int i=I*rect_length; i< i_end; ++i) {
-                C[i+j*M] += A[i+k*M]*B[k+j*M];
+          int j_end = ((J+1)*block_size < M? block_size : (M-(J*block_size)));
+          for(int j=0;j< j_end; ++j) {
+            int Cind = (I*rect_length)+(J*block_size+j)*M;
+            int k_end = ((K+1)*block_size < M? block_size : (M-(K*block_size)));
+            for(int k=0; k<k_end; ++k) {
+              int Aind = (I*rect_length)+(K*block_size+k)*M;
+              int Bind = (K*block_size)+k+(J*block_size+j)*M;
+              int i_end = ((I+1)*rect_length < M? rect_length : (M-(I*rect_length)));
+              for(int i=0; i< i_end; ++i) {
+                C[Cind+i] += A[Aind+i]*B[Bind];
               }
             }
           }
@@ -36,24 +39,3 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
     }
   }
 }
-
-//// Rectangular column sub-section type
-//void square_dgemm(const int M, const double* restrict A, const double* restrict B, double* restrict C)
-//{
-//  const int n_block = M / block_size + (M%block_size? 1 : 0);
-//  
-//  for(int J=0; J<n_block; ++J) {
-//    for(int K=0; K<n_block; ++K) {
-//      int j_end = ((J+1)*block_size < M? (J+1)*block_size : M);
-//      for(int j=J*block_size; j<j_end; ++j) {
-//        int k_end = ((K+1)*block_size < M? (K+1)*block_size : M);
-//        for(int k=K*block_size; k<k_end; ++k) {
-//          for(int i=0; i<M; ++i) {
-//          C[i+j*M] += A[i+k*M]*B[k+j*M];
-//          }
-//        }
-//      }
-//    }
-//  }
-//  
-//}
