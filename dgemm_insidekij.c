083c02f1c2666c4318827c75e57c97fc584e13bd
Elliot Cartee <evc34@cornell.edu>
2015-09-15 15:44:24 -0400

    Changed outside loop ordering

--- dgemm_insidekij.c
+++ dgemm_insidekij.c
@@ -1,4 +1,4 @@
-const char* dgemm_desc = "Blocked dgemm with loop ordering k,i,j (for blocks) and k,i,j (within blocks).";
+const char* dgemm_desc = "Blocked dgemm with loop ordering k,j,i (for blocks) and k,i,j (within blocks).";
 
 #ifndef BLOCK_SIZE
 #define BLOCK_SIZE ((int) 16)
@@ -42,10 +42,10 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
     int bi, bj, bk;
     for (bk = 0; bk < n_blocks; ++bk) {
         const int k = bk * BLOCK_SIZE;
-        for (bi = 0; bi < n_blocks; ++bi) {
-            const int i = bi * BLOCK_SIZE;
-            for (bj = 0; bj < n_blocks; ++bj) {
-                const int j = bj * BLOCK_SIZE;
+        for (bj = 0; bj < n_blocks; ++bj) {
+            const int j = bj * BLOCK_SIZE;
+            for (bi = 0; bi < n_blocks; ++bi) {
+                const int i = bi * BLOCK_SIZE;
                 do_block(M, A, B, C, i, j, k);
             }
         }
