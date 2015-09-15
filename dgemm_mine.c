22d530a155c6210651d5dd7d26c2ffe07c1a0fe3
Robert Chiodi <rmc298@cornell.edu>
2015-09-15 15:07:04 -0400

    Try setting alignments

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -14,13 +14,20 @@ const char* dgemm_desc = "My super awesome dgemm.";
 // Blocked type
 void square_dgemm(const int M, const double* restrict A, const double* restrict B, double* restrict C)
 {
+  double LHS[rect_length] __attribute__((aligned(16)));
+  double RHSA[rect_length] __attribute__((aligned(16)));
+  double RHSB;
+  
+  __assume(n1%16==0)
+  __assume(n2%16==0)
+
   const int n_rect = M/rect_length + (M%rect_length? 1 : 0);
   const int n_blocks = M / block_size + (M%block_size? 1 : 0);
   
   // Block loop
   for(int J=0; J<n_blocks; ++J) {
     for(int K=0; K<n_blocks; ++K) {
-      for(int I=0; I<n_rect; ++I){
+      for(int I=0; I<n_rect; ++I) {
         //Inner block loop
           int j_end = ((J+1)*block_size < M? block_size : (M-(J*block_size)));
           for(int j=0;j< j_end; ++j) {
@@ -28,14 +35,21 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
             int k_end = ((K+1)*block_size < M? block_size : (M-(K*block_size)));
             for(int k=0; k<k_end; ++k) {
               int Aind = (I*rect_length)+(K*block_size+k)*M;
-              int Bind = (K*block_size)+k+(J*block_size+j)*M;
+              RHSB = B[(K*block_size)+k+(J*block_size+j)*M];
               int i_end = ((I+1)*rect_length < M? rect_length : (M-(I*rect_length)));
-              for(int i=0; i< i_end; ++i) {
-                C[Cind+i] += A[Aind+i]*B[Bind];
+              __assume(
+              for(int i=0; i<rect_length; ++i) {
+                RHSA[i] = A[Aind+i];
+              }
+              for(int i=0; i<rect_length; ++i) {
+                LHS[i] = RHSA[i]*RHSB;
+              }
+              for(int i=0; i<i_end; ++i) {
+                C[Cind+i] += LHS[i];
+              }
               }
             }
           }
       }
     }
-  }
 }
