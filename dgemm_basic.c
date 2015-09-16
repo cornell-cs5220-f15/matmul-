6ccb6379feeaf703099da0777d8ce83b35f91522
sjm324 <sjm324@en-cs-totient-01.coecis.cornell.edu>
2015-09-16 18:29:31 -0400

    just kidding, working in wrong file

--- dgemm_basic.c
+++ dgemm_basic.c
@@ -1,12 +1,8 @@
 const char* dgemm_desc = "Basic, three-loop dgemm.";
 
-#define PERMUTATION 1
-
 void square_dgemm(const int M, 
                   const double *A, const double *B, double *C)
 {
-#if PERMUTATION == 0
-  // i, then j, then k
   int i, j, k;
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
@@ -16,15 +12,4 @@ void square_dgemm(const int M,
             C[j*M+i] = cij;
         }
     }
-#elif PERMUTATION == 1
-  // i, then k, then j
-#elif PERMUTATION == 2
-  // etc.....
-#elif PERMUTATION == 3
-
-#elif PERMUTATION == 4
-
-#else // PERUMTATION == 5
-
-#endif
 }
--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,7 +1,9 @@
 const char* dgemm_desc = "My awesome dgemm.";
 
-void square_dgemm(const int M, const double *A, const double *B, double *C)
-{
+#define PERMUTATION 1
+
+void square_dgemm(const int M, const double *A, const double *B, double *C) {
+#if PERMUTATION == 0
     int i, j, k;
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
@@ -11,4 +13,15 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
             C[j*M+i] = cij;
         }
     }
+#elif PERMUTATION == 1
+  // i, then k, then j
+#elif PERMUTATION == 2
+  // etc.....
+#elif PERMUTATION == 3
+
+#elif PERMUTATION == 4
+
+#else // PERUMTATION == 5
+
+#endif
 }
