2ee8c38d2b761008eb3d8413dd9b9569e35c522b
sjm324 <sjm324@en-cs-totient-01.coecis.cornell.edu>
2015-09-16 18:14:40 -0400

    ok checkout dgemm_basic.c and run multiple trials, just change the #define at the top to be what you want to plot for that run

--- dgemm_basic.c
+++ dgemm_basic.c
@@ -1,9 +1,13 @@
 const char* dgemm_desc = "Basic, three-loop dgemm.";
 
+#define PERMUTATION 1
+
 void square_dgemm(const int M, 
                   const double *A, const double *B, double *C)
 {
-    int i, j, k;
+#if PERMUTATION == 0
+  // i, then j, then k
+  int i, j, k;
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
             double cij = C[j*M+i];
@@ -12,4 +16,15 @@ void square_dgemm(const int M,
             C[j*M+i] = cij;
         }
     }
+#elif PERMUTATION == 1
+  // i, then k, then j
+#elif PERMUTATION == 2
+  // etc.....
+#elif PERMUTATION == 3
+
+#elif PERMUTATION == 4
+
+#else // PERUMTATION == 5
+
+#endif
 }
