e4cf3e467d926eb100000a4332c0c7f2501d40af
Elliot Cartee <evc34@cornell.edu>
2015-09-16 11:43:35 -0400

    Improving copy optimization and adding job file

--- dgemm_copyopt_transpose.c
+++ dgemm_copyopt_transpose.c
@@ -1,4 +1,4 @@
-const char* dgemm_desc = "Blocked dgemm with copy optimization. Inside loop ordering is j,k,i. Outside loop ordering is i,k,j";
+const char* dgemm_desc = "Blocked dgemm with copy optimization (transposing the copy). Not yet finished. Inside loop ordering is j,k,i.";
 
 #ifndef BLOCK_SIZE
 #define BLOCK_SIZE ((int) 16)
@@ -71,12 +71,12 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
     const int n_blocks = M / BLOCK_SIZE + (M%BLOCK_SIZE? 1 : 0);
     int bi, bj, bk;
-    for (bi = 0; bi < n_blocks; ++bi) {
-        const int i = bi * BLOCK_SIZE;
-        for (bk = 0; bk < n_blocks; ++bk) {
-            const int k = bk * BLOCK_SIZE;
-            for (bj = 0; bj < n_blocks; ++bj) {
-                const int j = bj * BLOCK_SIZE;
+    for (bk = 0; bk < n_blocks; ++bk) {
+        const int k = bk * BLOCK_SIZE;
+        for (bj = 0; bj < n_blocks; ++bj) {
+            const int j = bj * BLOCK_SIZE;
+            for (bi = 0; bi < n_blocks; ++bi) {
+                const int i = bi * BLOCK_SIZE;
                 do_block(M, A, B, C, i, j, k);
             }
         }
