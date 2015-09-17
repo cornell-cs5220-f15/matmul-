1152cbb57dd292cf6f71c87bbe874ecd46ab2186
Marc Aurele Tiberius Gilles <mtg79@en-cs-totient-01.coecis.cornell.edu>
2015-09-17 11:08:12 -0400

    L2 block,10L2

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -2,26 +2,41 @@ const char* dgemm_desc = "My awesome matmul.";
 
 #include <stdlib.h>
 
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 16)
+#ifndef L1_BS
+#define L1_BS ((int) 16)
 #endif
+
+#ifndef L2_BS
+#define L2_BS ((int) 16)
+#endif
+
+#ifndef L3_BS
+#define L3_BS ((int) 10)
+#endif
+
+
 /*
+
   A,B,C are M-by-M
+  L1_BS is the size of sub matrix that will fit into L1
+  L2_BS is the number of submatrix that will fit in L2
+  L3*L2 is the number of submatrix that will fit in L3
+
 */
 void row_to_block(const int M, const int nblock, const double *A, double *newA)
 {
-	// converts to block indexing and pads the new matrix with zeros so that it is divisble by BLOCK_SIZE
+	// converts to block indexing and pads the new matrix with zeros so that it is divisble by L1_BS
 	int bi,bj,i,j;	
 	for(bi=0; bi < nblock; ++bi){
 		for(bj=0; bj < nblock; ++bj){
-			for(i=0; i < BLOCK_SIZE; ++i){
-				for(j=0; j < BLOCK_SIZE; ++j){
-					if ((bj*BLOCK_SIZE+j) >= M || bi*BLOCK_SIZE+i >= M){ 
+			for(i=0; i < L1_BS; ++i){
+				for(j=0; j < L1_BS; ++j){
+					if ((bj*L1_BS+j) >= M || bi*L1_BS+i >= M){ 
 						// we can optimize this to delete this "if". this is doing the padding 
-						newA[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]=0;
+						newA[((bj*nblock+bi)*L1_BS*L1_BS+ i*L1_BS+j)]=0;
 					}
 					else{
-						newA[(bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j]=A[(j+bj*BLOCK_SIZE)*M+bi*BLOCK_SIZE+i]; 
+						newA[(bj*nblock+bi)*L1_BS*L1_BS+ i*L1_BS+j]=A[(j+bj*L1_BS)*M+bi*L1_BS+i]; 
 					}				
 				}
 			}
@@ -34,13 +49,13 @@ void block_to_row(const int M, const int nblock, double *A, const double *newA)
 	int bi, bj,i,j;	
 	for(bi=0; bi < nblock; ++bi){
 		for(bj=0; bj < nblock; ++bj){
-			for(i=0; i < BLOCK_SIZE; ++i){
-				for(j=0; j < BLOCK_SIZE; ++j){
-					if ((bj*BLOCK_SIZE+j)>= M || bi*BLOCK_SIZE+i >= M){
+			for(i=0; i < L1_BS; ++i){
+				for(j=0; j < L1_BS; ++j){
+					if ((bj*L1_BS+j)>= M || bi*L1_BS+i >= M){
 					   	// we can optimize this to delete this "if". this is doing the padding 
 					}
 					else{
-						A[(j+bj*BLOCK_SIZE)*M+bi*BLOCK_SIZE+i]=newA[(bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j];
+						A[(j+bj*L1_BS)*M+bi*L1_BS+i]=newA[(bj*nblock+bi)*L1_BS*L1_BS+ i*L1_BS+j];
 				   	}
 
 				}
@@ -54,13 +69,13 @@ void do_block(const int M, const int nblock,
               const int bi, const int bj, const int bk)
 {
 	int i, j, k;
-    for (i = 0; i < BLOCK_SIZE; ++i) {
-        for (j = 0; j < BLOCK_SIZE; ++j) {
-            double cij = C[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)];
-            for (k = 0; k < BLOCK_SIZE; ++k) {
-                cij += A[((bk*nblock+bi))*BLOCK_SIZE*BLOCK_SIZE+BLOCK_SIZE*i+k] * B[((bj*nblock)+bk)*BLOCK_SIZE*BLOCK_SIZE+BLOCK_SIZE*k+j];
+    for (i = 0; i < L1_BS; ++i) {
+        for (j = 0; j < L1_BS; ++j) {
+            double cij = C[((bj*nblock+bi)*L1_BS*L1_BS+ i*L1_BS+j)];
+            for (k = 0; k < L1_BS; ++k) {
+                cij += A[((bk*nblock+bi))*L1_BS*L1_BS+L1_BS*i+k] * B[((bj*nblock)+bk)*L1_BS*L1_BS+L1_BS*k+j];
             }
-            C[((bj*nblock+bi)*BLOCK_SIZE*BLOCK_SIZE+ i*BLOCK_SIZE+j)]= cij;
+            C[((bj*nblock+bi)*L1_BS*L1_BS+ i*L1_BS+j)]= cij;
         }
     }
 }
@@ -71,17 +86,29 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
 	   block i= row index of block
 	   block j = column index of block
 	   nblock = number of blocks
-	   */
-	int pad_size, bi, bj, bk, nblock;
+	   L2bi, L2bj index of L2
+           L2nblock= number of L1block that fit into L2
+	   L2rem number of remaining blocks 
+	*/
+	int pad_size, bi, bj, bk, L2bi, L2bj, L2bk, nblock, L2nblock;
 
-	if (M%BLOCK_SIZE==0){
-		nblock=M/BLOCK_SIZE;
+	if (M%L1_BS==0){
+		nblock=M/L1_BS;
 		pad_size=M;
 	}
 	else{ 
-		pad_size=((M/BLOCK_SIZE)+1)*BLOCK_SIZE;
-		nblock=M/BLOCK_SIZE+1;
+		pad_size=((M/L1_BS)+1)*L1_BS;
+		nblock=M/L1_BS+1;
 	}
+	
+	// number of L2
+
+	if(pad_size%(L2_BS*L1_BS)==0){
+	L2nblock=pad_size/(L2_BS*L1_BS);}
+
+	else{L2nblock=pad_size/(L2_BS*L1_BS)+1;}
+
+		
 
 	double *bA= (double*) malloc(pad_size*pad_size*sizeof(double));
 	double *bB= (double*) malloc(pad_size*pad_size*sizeof(double));
@@ -91,14 +118,23 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
 	row_to_block(M,nblock, A, bA);
 	row_to_block(M,nblock, B, bB);
 	row_to_block(M,nblock, C, bC);
+	
 
-	for (bi = 0; bi < nblock; ++bi) {
-		for (bj = 0; bj < nblock; ++bj) {
-			for (bk = 0; bk < nblock; ++bk) {
-				do_block(M, nblock, bA, bB, bC, bi, bj, bk);
+	for (L2bi=0; L2bi < L2nblock; ++L2bi){  
+	for (L2bj=0; L2bj < L2nblock; ++L2bj){
+	for (L2bk=0; L2bk < L2nblock; ++L2bk){
+	for (bi = 0; bi < L2_BS; ++bi) {
+		for (bj = 0; bj < L2_BS; ++bj) {
+			for (bk = 0; bk < L2_BS; ++bk) {
+				//printf("%d, %d, %d",L2bi*L2_BS+bi, L2bj*L2_BS+bj, L2bk*L2_BS+bk);
+				if ((L2bi*L2_BS+bi <  nblock) && (L2bj*L2_BS+bj < nblock) && (L2bk*L2_BS +bk <nblock)){
+				do_block(M, nblock, bA, bB, bC, L2bi*L2_BS+bi, L2bj*L2_BS+bj, L2bk*L2_BS+bk);}
 			}
 		}
 	}
-	// reindex back to column
+	}
+	}
+	}
+// reindex back to column
 	block_to_row(M,nblock,C,bC);
 }
