11dd469b95995d38bee9ee4ceefab14b57997f24
Yalcin Ozhabes <ao294@cornell.edu>
2015-09-16 23:54:59 -0400

    cache optimization with 3 layers.

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,13 +1,17 @@
 
 
-const char* dgemm_desc = "My 2 level blocked dgemm.";
+const char* dgemm_desc = "My 3 level blocked dgemm.";
 
 #ifndef L3_BLOCK_SIZE
-#define L3_BLOCK_SIZE ((int) 250)
+#define L3_BLOCK_SIZE ((int) 15)
 #endif
 
 #ifndef L2_BLOCK_SIZE
-#define L2_BLOCK_SIZE ((int) 125)
+#define L2_BLOCK_SIZE ((int) 10)
+#endif
+
+#ifndef L1_BLOCK_SIZE
+#define L1_BLOCK_SIZE ((int) 5)
 #endif
 
 #include <stdio.h>
@@ -27,10 +31,14 @@ void basic_dgemm(const int lda, const int M, const int N, const int K,
             double cij = C[j*lda+i];
             for (k = 0; k < K; ++k) {
                 cij += A[k*lda+i] * B[j*lda+k];
+                if(i == 0 && j == 0){
+                    printf("C(0,0) += A(%d,%d) * B(%d,%d) = %.3f %.3f\n", i, k, k, j, A[k*lda+i], B[j*lda+k]);
+                }
             }
             C[j*lda+i] = cij;
         }
     }
+    printf("----------------------------------------\n");
 }
 
 // void do_block_L1(const int lda,
@@ -44,31 +52,35 @@ void basic_dgemm(const int lda, const int M, const int N, const int K,
 //                 A + i + k*lda, B + k + j*lda, C + i + j*lda);
 // }
 //
-// void do_block_L2(const int lda,
-//               const double *A, const double *B, double *C,
-//               const int ii, const int jj, const int kk)
-// {
-//
-//     const int M = (i+L3_BLOCK_SIZE > lda? lda-i : L3_BLOCK_SIZE);
-//     const int N = (j+L3_BLOCK_SIZE > lda? lda-j : L3_BLOCK_SIZE);
-//     const int K = (k+L3_BLOCK_SIZE > lda? lda-k : L3_BLOCK_SIZE);
-//
-//     const int nL2_row_blocks_A = M / L2_BLOCK_SIZE + (M%L2_BLOCK_SIZE? 1 : 0);
-//     const int nL2_col_blocks_A = K / L2_BLOCK_SIZE + (K%L2_BLOCK_SIZE? 1 : 0);
-//     const int nL2_col_blocks_B = N / L2_BLOCK_SIZE + (N%L2_BLOCK_SIZE? 1 : 0);
-//
-//     int bi, bj, bk;
-//     for (bi = 0; bi < nL2_row_blocks_A; ++bi) {
-//         const int i = bi * L2_BLOCK_SIZE + ii;
-//         for (bj = 0; bj < nL2_col_blocks_B; ++bj) {
-//             const int j = bj * L2_BLOCK_SIZE + ii;
-//             for (bk = 0; bk < nL2_col_blocks_A; ++bk) {
-//                 const int k = bk * L2_BLOCK_SIZE + ii;
-//                 do_block_L2(lda, A + ii + i + (kk+ k)*lda, B + kk + k + (jj+j)*lda, C + ii+ i +(jj + j)*lda);
-//             }
-//         }
-//     }
-// }
+void do_block_L2(const int lda,
+              const double *A, const double *B, double *C,
+              const int ii, const int jj, const int kk)
+{
+
+    const int MM = (ii+L2_BLOCK_SIZE > lda? lda-ii : L2_BLOCK_SIZE);
+    const int KK = (kk+L2_BLOCK_SIZE > lda? lda-kk : L2_BLOCK_SIZE);
+    const int NN = (jj+L2_BLOCK_SIZE > lda? lda-jj : L2_BLOCK_SIZE);
+
+    const int nL1_row_blocks_A = MM / L1_BLOCK_SIZE + (MM % L1_BLOCK_SIZE? 1 : 0);
+    const int nL1_col_blocks_A = KK / L1_BLOCK_SIZE + (KK % L1_BLOCK_SIZE? 1 : 0);
+    const int nL1_col_blocks_B = NN / L1_BLOCK_SIZE + (NN % L1_BLOCK_SIZE? 1 : 0);
+
+    int bi, bj, bk;
+    for (bi = 0; bi < nL1_row_blocks_A; ++bi) {
+        const int i = bi * L1_BLOCK_SIZE + ii;
+        const int M = (i+L1_BLOCK_SIZE > lda? lda-i : L1_BLOCK_SIZE);
+        for (bj = 0; bj < nL1_col_blocks_B; ++bj) {
+            const int j = bj * L1_BLOCK_SIZE + jj;
+            const int N = (j+L1_BLOCK_SIZE > lda? lda-j : L1_BLOCK_SIZE);
+            for (bk = 0; bk < nL1_col_blocks_A; ++bk) {
+                const int k = bk * L1_BLOCK_SIZE + kk;
+                const int K = (k+L1_BLOCK_SIZE > lda? lda-k : L1_BLOCK_SIZE);
+                basic_dgemm(lda, M, N, K,
+                            A + i + k*lda, B + k + j*lda, C + i + j*lda);
+            }
+        }
+    }
+}
 
 void do_block_L3(const int lda,
               const double *A, const double *B, double *C,
@@ -86,15 +98,11 @@ void do_block_L3(const int lda,
     int bi, bj, bk;
     for (bi = 0; bi < nL2_row_blocks_A; ++bi) {
         const int i = bi * L2_BLOCK_SIZE + ii;
-        const int M = (i+L2_BLOCK_SIZE > lda? lda-i : L2_BLOCK_SIZE);
         for (bj = 0; bj < nL2_col_blocks_B; ++bj) {
             const int j = bj * L2_BLOCK_SIZE + jj;
-            const int N = (j+L2_BLOCK_SIZE > lda? lda-j : L2_BLOCK_SIZE);
             for (bk = 0; bk < nL2_col_blocks_A; ++bk) {
                 const int k = bk * L2_BLOCK_SIZE + kk;
-                const int K = (k+L2_BLOCK_SIZE > lda? lda-k : L2_BLOCK_SIZE);
-                basic_dgemm(lda, M, N, K,
-                            A + i + k*lda, B + k + j*lda, C + i + j*lda);
+                do_block_L2(lda, A, B, C, i, j, k);
             }
         }
     }
@@ -116,4 +124,5 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
             }
         }
     }
+    getchar();
 }
