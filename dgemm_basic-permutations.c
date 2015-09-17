3b500e60fe0c681703341dadaa52b808cb08535e
sjm324 <sjm324@en-cs-totient-01.coecis.cornell.edu>
2015-09-17 01:50:06 -0400

    segfaults on complex transpose removed and ignored. added basic-permutations separately, there is slight problem with the one that hsould be the best

--- dgemm_basic-permutations.c
+++ dgemm_basic-permutations.c
@@ -1,23 +1,8 @@
-const char* dgemm_desc = "My awesome dgemm.";
+const char* dgemm_desc = "Basic loop dgemm permutations";
 
 #define PERMUTATION 5
 
-inline void naive_transpose(int M, double* mat) {
-    int c, r;
-    double tmp;
-    for(int c = 0; c < M; ++c) {
-        for(int r = 0; r < M; ++r) {
-            tmp = mat[c*M+r];
-            mat[c*M+r] = mat[r*M+c];
-            mat[r*M+c] = tmp;
-        }
-    }
-}
-
 void square_dgemm(const int M, const double *A, const double *B, double *C) {
-    // why did you give us const?  aren't we supposed to play with this?
-    double* bad_idea = (double *)(&(*A));
-    naive_transpose(M, bad_idea);
 #if PERMUTATION == 0
     int i, j, k;
     for (i = 0; i < M; ++i) {
@@ -86,5 +71,4 @@ void square_dgemm(const int M, const double *A, const double *B, double *C) {
         }
     }
 #endif
-    naive_transpose(M, bad_idea);
 }
