21aeaa3abd239830f4cd507d974bd879815bb7a5
Alan Young Cheng <alan7cheng@gmail.com>
2015-09-17 05:36:09 -0400

    changed dgemm_mine to a version that transposes matrix A

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,76 +1,31 @@
-const char* dgemm_desc = "My awesome dgemm.";
+#include <stdlib.h>
 
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 16)
-#endif
+const char* dgemm_desc = "Basic, three-loop dgemm.";
 
-#ifdef OSX_ACCELERATE
-# include <Acclerate/Accelerate.h>
-#else
-# include <cblas.h>
-#endif
+void make_transpose(const int M, const double *A, double *out)
+{
+    int i, j;
+    for (i = 0; i < M; ++i) {
+        for (j = 0; j < M; ++j) {
+            out[j*M + i] = A[i*M + j];
+        }
+    }
+}
 
-/*
- *   A is M-by-K
- *   B is K-by-N
- *   C is M-by-N
- *   lda is the leading dimension of the matrix (the M of square_dgemm).
- */
-void basic_dgemm(const int lda, const int M, const int N, const int K,
-                 const double *A, const double *B, double *C)
+
+void square_dgemm(const int M, 
+                  const double *A, const double *B, double *C)
 {
     int i, j, k;
+    double *A_T = (double*)malloc(M * M * sizeof(double));
+    make_transpose(M, A, A_T);
     for (i = 0; i < M; ++i) {
-        for (j = 0; j < N; ++j) {
-            double cij = C[j*lda+i];
-            for (k = 0; k < K; ++k) {
-                cij += A[k*lda+i] * B[j*lda+k];
-            }
-            C[j*lda+i] = cij;
+        for (j = 0; j < M; ++j) {
+            double cij = C[j*M+i];
+            for (k = 0; k < M; ++k)
+                cij += A_T[i*M+k] * B[j*M+k];
+            C[j*M+i] = cij;
         }
     }
-}
-
-void blas_dgemm(const int lda, const int M, const int N, const int K,
-		const double *A, const double *B, double *C)
-{
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
-            M, N, K,
-            1.0, A, lda, B, lda, 1.0, C, lda);
-}
-
-void do_block(const int lda,
-              const double *A, const double *B, double *C,
-              const int i, const int j, const int k)
-{
-    const int M = (i+BLOCK_SIZE > lda? lda-i : BLOCK_SIZE);
-    const int N = (j+BLOCK_SIZE > lda? lda-j : BLOCK_SIZE);
-    const int K = (k+BLOCK_SIZE > lda? lda-k : BLOCK_SIZE);
-    blas_dgemm(lda, M, N, K,
-                A + i + k*lda, B + k + j*lda, C + i + j*lda);
-}
-
-void clear(const int M, double *C)
-{
-    int i;
-    for (i = 0; i < M * M; ++i) {
-        C[i] = 0;
-    }
-}
-
-void square_dgemm(const int M, const double *A, const double *B, double *C)
-{
-    //clear(M, C);
-    const int n_blocks = M / BLOCK_SIZE + (M%BLOCK_SIZE? 1 : 0);
-    int bi, bj, bk;
-    for (bi = 0; bi < n_blocks; ++bi) {
-        const int i = bi * BLOCK_SIZE;
-        for (bj = 0; bj < n_blocks; ++bj) {
-            const int j = bj * BLOCK_SIZE;
-            for (bk = 0; bk < n_blocks; ++bk) {
-                const int k = bk * BLOCK_SIZE;
-                do_block(M, A, B, C, i, j, k);
-            }
-        }
-    }
+    free(A_T);
 }
