dd60a5edb845013ea7d45f3acabb17fe7019f01f
sjm324 <sjm324@en-cs-totient-01.coecis.cornell.edu>
2015-09-17 17:27:34 -0400

    did that stash pop actually work

--- dgemm_basic-permutations.c
+++ dgemm_basic-permutations.c
@@ -1,72 +1,74 @@
 const char* dgemm_desc = "Basic loop dgemm permutations";
 
+// more convenient access
+#define A(i, j) A[j*M + i]
+#define B(i, j) B[j*M + i]
+#define C(i, j) C[j*M + i]
+
 #define PERMUTATION 5
 
 void square_dgemm(const int M, const double *A, const double *B, double *C) {
-#if PERMUTATION == 0
     int i, j, k;
+#if PERMUTATION == 0
+    // i, then j, then k
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
-            double cij = C[j*M+i];
-            for (k = 0; k < M; ++k)
-                cij += A[k*M+i] * B[j*M+k];
-            C[j*M+i] = cij;
+            double c_ij = C(i, j);
+            for (k = 0; k < M; ++k) {
+                c_ij += A(i, k) * B(i, j);
+            }
+            C(i, j) = c_ij;
         }
     }
 #elif PERMUTATION == 1
     // i, then k, then j
-    int i, j, k;
     for(i = 0; i < M; ++i) {
         for(k = 0; k < M; ++k) {
-            double aki = A[k*M+i];
+            double a_ik = A(i, k);
             for(j = 0; j < M; ++j) {
-                C[j*M+i] += aki * B[j*M+k];
+                C(i, j) += a_ik * B(k, j);
             }
         }
     }
 #elif PERMUTATION == 2
     // j, then i, then k
-    int i, j, k;
     for(j = 0; j < M; ++j) {
         for(i = 0; i < M; ++i) {
-            double cij = C[j*M+i];
+            double c_ij = C(i, j);
             for(k = 0; k < M; ++k) {
-                cij += A[k*M+i] * B[j*M+k];
+                c_ij += A(i, k) * B(k, j);
             }
-            C[j*M+i] = cij;
+            C(i, j) = c_ij;
         }
     }
 
 #elif PERMUTATION == 3
     // j, then k, then i
-    int i, j, k;
     for(j = 0; j < M; ++j) {
         for(k = 0; k < M; ++k) {
-            double bjk = B[j*M+k];
+            double b_kj = B(k, j);
             for(i = 0; i < M; ++i) {
-                C[j*M+i] += A[k*M+i] * bjk;
+                C(i, j) += A(i, k) * b_kj;
             }
         }
     }
 #elif PERMUTATION == 4
     // k, then i, then j
-    int i, j, k;
     for(k = 0; k < M; ++k) {
         for(i = 0; i < M; ++i) {
-            double aki = A[k*M+i];
+            double a_ik = A(i, k);
             for(j = 0; j < M; ++j) {
-                C[j*M+i] += aki * B[j*M+k];
+                C(i, j) += a_ik * B(k, j);
             }
         }
     }
 #else // PERUMTATION == 5
     // k, then j, then i
-    int i, j, k;
     for(k = 0; k < M; ++k) {
         for(j = 0; j < M; ++j) {
-            double bjk = B[j*M+k];
+            double b_kj = B(k, j);
             for(i = 0; i < M; ++i) {
-                C[j*M+i] += A[k*M+i] * bjk;
+                C(i, j) += A(i, k) * bjk;
             }
         }
     }
