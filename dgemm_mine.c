f98d3c3923332eb9b3ee0776e842b5bce960ef05
calvinwylie <calvin.wylie@gmail.com>
2015-09-17 16:16:36 -0400

    Added timing plots and further work on subblocking

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -4,16 +4,56 @@ const char* dgemm_desc = "My awesome dgemm.";
 #define BLOCK_SIZE ((int) 64) // Multiples of 8 bytes!
 #endif
 
-void block_dgemm(const double *restrict A, 
-                 const double *restrict B, 
-                 double *restrict C)
+#ifndef SUBBLOCK_SIZE
+#define SUBBLOCK_SIZE ((int) 8) // Multiples of BLOCK_SIZE!
+#endif
+
+#define NUM_SUBBLOCKS BLOCK_SIZE / SUBBLOCK_SIZE
+
+void sublock_dgemm(const double *restrict A, 
+                   const double *restrict B, 
+                   double *restrict C)
 {
     int i, j, k;
-    for (j = 0; j < BLOCK_SIZE; ++j) {
-        for (k = 0; k < BLOCK_SIZE; ++k) {
+    for (j = 0; j < SUBBLOCK_SIZE; ++j) {
+        for (k = 0; k < SUBBLOCK_SIZE; ++k) {
             double bkj = B[j*BLOCK_SIZE + k];
-            for (i = 0; i < BLOCK_SIZE; ++i) {
-                C[j*BLOCK_SIZE + i] += A[k*BLOCK_SIZE + i] * bkj;
+            C[j*BLOCK_SIZE] += A[k*BLOCK_SIZE] * bkj;
+            C[j*BLOCK_SIZE + 1] += A[k*BLOCK_SIZE + 1] * bkj;
+            C[j*BLOCK_SIZE + 2] += A[k*BLOCK_SIZE + 2] * bkj;
+            C[j*BLOCK_SIZE + 3] += A[k*BLOCK_SIZE + 3] * bkj;
+            C[j*BLOCK_SIZE + 4] += A[k*BLOCK_SIZE + 4] * bkj;
+            C[j*BLOCK_SIZE + 5] += A[k*BLOCK_SIZE + 5] * bkj;
+            C[j*BLOCK_SIZE + 6] += A[k*BLOCK_SIZE + 6] * bkj;
+            C[j*BLOCK_SIZE + 7] += A[k*BLOCK_SIZE + 7] * bkj;
+        }
+    }
+}
+
+void do_block(const double *restrict A, 
+              const double *restrict B, 
+              double *restrict C)
+{
+    // int i, j, k;
+    // for (j = 0; j < BLOCK_SIZE; ++j) {
+    //     for (k = 0; k < BLOCK_SIZE; ++k) {
+    //         double bkj = B[j*BLOCK_SIZE + k];
+    //         for (i = 0; i < BLOCK_SIZE; ++i) {
+    //             C[j*BLOCK_SIZE + i] += A[k*BLOCK_SIZE + i] * bkj;
+    //         }
+    //     }
+    // }
+    
+    int bi, bj, bk;
+    for (bi = 0; bi < NUM_SUBBLOCKS; ++bi) {
+        const int i = bi * SUBBLOCK_SIZE;
+        for (bj = 0; bj < NUM_SUBBLOCKS; ++bj) {
+            const int j = bj * SUBBLOCK_SIZE;
+            for (bk = 0; bk < NUM_SUBBLOCKS; ++bk) {
+                const int k = bk * SUBBLOCK_SIZE;
+                sublock_dgemm( A + i + k*BLOCK_SIZE,
+                               B + k + j*BLOCK_SIZE,
+                               C + i + j*BLOCK_SIZE );
             }
         }
     }
@@ -47,27 +87,28 @@ void square_dgemm(const int lda,
     for (bi = 0; bi < n_blocks; ++bi) {
         const int i = bi * BLOCK_SIZE;
         const int M = (i+BLOCK_SIZE > lda? lda-i : BLOCK_SIZE);
-        for (bj = 0; bj < n_blocks; ++bj) {
-            const int j = bj * BLOCK_SIZE;
-            const int N = (j+BLOCK_SIZE > lda? lda-j : BLOCK_SIZE);
 
-            realloc_block(lda, BLOCK_SIZE, C + i + j*lda, C_temp, M, N);
+        for (bk = 0; bk < n_blocks; ++bk) {
+            const int k = bk * BLOCK_SIZE;
+            const int K = (k+BLOCK_SIZE > lda? lda-k : BLOCK_SIZE);
 
-            for (bk = 0; bk < n_blocks; ++bk) {
-                const int k = bk * BLOCK_SIZE;
-                const int K = (k+BLOCK_SIZE > lda? lda-k : BLOCK_SIZE);
-                
-                realloc_block(lda, BLOCK_SIZE, A + i + k*lda, A_temp, M, K);
+            realloc_block(lda, BLOCK_SIZE, A + i + k*lda, A_temp, M, K);
+
+            for (bj = 0; bj < n_blocks; ++bj) {
+                const int j = bj * BLOCK_SIZE;
+                const int N = (j+BLOCK_SIZE > lda? lda-j : BLOCK_SIZE);
+    
                 realloc_block(lda, BLOCK_SIZE, B + k + j*lda, B_temp, K, N);
-                
-                block_dgemm(A_temp, B_temp, C_temp);
-            }
+                realloc_block(lda, BLOCK_SIZE, C + i + j*lda, C_temp, M, N);
+                    
+                do_block(A_temp, B_temp, C_temp);
 
-            // copy results back into main C array
-            int ci, cj;
-            for (ci = 0; ci < M; ++ci) {
-                for (cj = 0; cj < N; ++cj) {
-                    C[(j + cj)*lda + (i+ci)] = C_temp[cj*BLOCK_SIZE + ci];
+                // copy results back into main C array
+                int ci, cj;
+                for (ci = 0; ci < M; ++ci) {
+                    for (cj = 0; cj < N; ++cj) {
+                        C[(j + cj)*lda + (i+ci)] = C_temp[cj*BLOCK_SIZE + ci];
+                    }
                 }
             }
         }
