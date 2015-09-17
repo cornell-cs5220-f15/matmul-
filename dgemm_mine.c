48231222954cdad30ded826aa60f57f1e4c42249
Lim Mingjie, Kenneth <me@kenlimmj.com>
2015-09-16 23:13:03 -0400

    Added loop ordering timings

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,27 +1,15 @@
-const char* dgemm_desc = "I can't do this anymore";
+const char* dgemm_desc = "Basic, three-loop dgemm.";
 
-void square_dgemm(const int M, const double *A, const double *B, double *C)
+void square_dgemm(const int M,
+                  const double *A, const double *B, double *C)
 {
-  int i, j, k;
-
-  for (i = 0; i < M; i += 2) {
-    for (j = 0; j < M; j += 2) {
-      double acc00 = 0;
-      double acc01 = 0;
-      double acc10 = 0;
-      double acc11 = 0;
-
-      for (k = 0; k < M; k++) {
-        acc00 += A[j+k*M]     * B[k+i*M];
-        acc01 += A[j+(k+1)*M] * B[k+i*M];
-        acc10 += A[j+k*M]     * B[k+1+i*M];
-        acc11 += A[j+(k+1)*M] * B[k+1+i*M];
-      }
-
-      C[j+i*M] = acc00;
-      C[j+(i+1)*M] = acc01;
-      C[j+1+i*M] = acc10;
-      C[j+1+(i+1)*M] = acc11;
+    int i, j, k;
+    for (j = 0; j < M; ++j) {
+        for (i = 0; i < M; ++i) {
+            double cij = C[j*M+i];
+            for (k = 0; k < M; ++k)
+                cij += A[k*M+i] * B[j*M+k];
+            C[j*M+i] = cij;
+        }
     }
-  }
 }
