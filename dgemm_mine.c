4a166fb8eebd6bdf60e4b0bc6a4bbfb926d62fe0
Bob_MAC <jtcdbob@gmail.com>
2015-09-22 15:38:26 -0400

    avx kernal added

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -12,6 +12,12 @@ const char* dgemm_desc = "My awesome dgemm.";
 #define SUB_BLOCK_SIZE ((int) 8)
 #endif
 
+#ifdef USE_SHUFPD
+#  define swap_sse_doubles(a) _mm_shuffle_pd(a, a, 1)
+#else
+#  define swap_sse_doubles(a) (__m128d) _mm_shuffle_epi32((__m128i) a, 0x4e)
+#endif
+
 /*
   A is M-by-K
   B is K-by-N
@@ -23,6 +29,7 @@ void basic_dgemm(const int lda, const int M, const int N, const int K,
                  const double* restrict A, const double* restrict B,
                  double* restrict C)//, const double restrict *C_original)
 {
+
     // New kernal function for A stored in row-major.
     int i, j, k;
     for(j = 0; j < N; ++j){
@@ -34,6 +41,53 @@ void basic_dgemm(const int lda, const int M, const int N, const int K,
         C[i + j*lda] = cij;
       }
     }
+}
+
+void mine_dgemm( const double* restrict A, const double* restrict B,
+                 double* restrict C){
+    // My kernal function that uses AVX for optimal performance
+    // It always assumes an input of A = 2 * P, B = P * 2
+    // Template from https://bitbucket.org/dbindel/cs5220-s14/wiki/sse
+
+    int P = 6;
+    // This is really implicit in using the aligned ops...
+    __assume_aligned(A, 16);
+    __assume_aligned(B, 16);
+    __assume_aligned(C, 16);
+
+    // Load diagonal and off-diagonals
+    __m128d cd = _mm_load_pd(C+0);
+    __m128d co = _mm_load_pd(C+2);
+
+    /*
+     * Do block dot product.  Each iteration adds the result of a two-by-two
+     * matrix multiply into the accumulated 2-by-2 product matrix, which is
+     * stored in the registers cd (diagonal part) and co (off-diagonal part).
+     */
+    for (int k = 0; k < P; k += 2) {
+
+        __m128d a0 = _mm_load_pd(A+2*k+0);
+        __m128d b0 = _mm_load_pd(B+2*k+0);
+        __m128d td0 = _mm_mul_pd(a0, b0);
+        __m128d bs0 = swap_sse_doubles(b0);
+        __m128d to0 = _mm_mul_pd(a0, bs0);
+
+        __m128d a1 = _mm_load_pd(A+2*k+2);
+        __m128d b1 = _mm_load_pd(B+2*k+2);
+        __m128d td1 = _mm_mul_pd(a1, b1);
+        __m128d bs1 = swap_sse_doubles(b1);
+        __m128d to1 = _mm_mul_pd(a1, bs1);
+
+        __m128d td_sum = _mm_add_pd(td0, td1);
+        __m128d to_sum = _mm_add_pd(to0, to1);
+
+        cd = _mm_add_pd(cd, td_sum);
+        co = _mm_add_pd(co, to_sum);
+    }
+
+    // Write back sum
+    _mm_store_pd(C+0, cd);
+    _mm_store_pd(C+2, co);
 }
 
 void do_block(const int lda,
--- matmul.c
+++ matmul.c
@@ -46,12 +46,12 @@ extern void square_dgemm();
   around some of the powers-of-two.
 */
 const int test_sizes[] = {
-    31,
+    2,
 
 #if defined(DEBUG_RUN)
-# define MAX_SIZE 31u//229u
+# define MAX_SIZE 2u//229u
 #else
-    32, 96, 97, 127, 128, 129, 191, 192, 229,
+    31, 32, 96, 97, 127, 128, 129, 191, 192, 229,
     255, 256, 257, 319, 320, 321, 417, 479, 480, 511, 512, 639, 640,
     767, 768, 769, 1023, 1024, 1025, 1525, 1526, 1527
 # define MAX_SIZE 1527u
