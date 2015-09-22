a4763870eec90931df4f2e08b87e0dc34a43e625
jtcdbob <jtcdbob@gmail.com>
2015-09-22 15:31:08 -0400

    mm_malloc

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -52,7 +52,7 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
 {
     // Preallocate a space for submatrices A, B and C
     // double* A_transposed = (double*) malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
-    double* A_transposed = (double*) _mm_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
+    double* A_transposed = (double*) _mm_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double),16);
     // Assign blocks for kernals to perform fast computation.
     const int n_blocks = M / BLOCK_SIZE + (M%BLOCK_SIZE? 1 : 0); // # of blocks
     int bi, bj, bk;
@@ -78,5 +78,5 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
 
       }
     }
-    free(A_transposed);
+    _mm_free(A_transposed);
 }
