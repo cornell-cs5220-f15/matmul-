2acfe63ffa02427233a4efc0cbc6fe2f27d8d9ec
Bob_MAC <jtcdbob@gmail.com>
2015-09-24 19:52:17 -0400

    why doesn't it work?

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -189,9 +189,9 @@ void mine_fma_dgemm( const double* restrict A, const double* restrict B,
 
     // // Store matrix C
     _mm256_storeu_pd(C + Matrix_size * 0, c0);
-    // _mm256_storeu_pd(C + Matrix_size * 1, c1);
-    // _mm256_storeu_pd(C + Matrix_size * 2, c2);
-    // _mm256_storeu_pd(C + Matrix_size * 3, c3);
+    _mm256_storeu_pd(C + Matrix_size * 1, c1);
+    _mm256_storeu_pd(C + Matrix_size * 2, c2);
+    _mm256_storeu_pd(C + Matrix_size * 3, c3);
 }
 
 
