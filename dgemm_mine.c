4ed3f136cf91d38c1c432cba6c38ed4e8c3fe1e5
Bob_MAC <jtcdbob@gmail.com>
2015-09-24 19:29:49 -0400

    testing_03

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -179,8 +179,8 @@ void mine_fma_dgemm( const double* restrict A, const double* restrict B,
       c3  = _mm256_fmadd_pd(a3, bij, c3); // C = A * B + C;
     }
 
-    // double * res = (double*)&c1;
-    // printf("td1 elements: %f\t%f\n", res[0], res[1]);
+    double * res = (double*)&c1;
+    printf("td1 elements: %f\t%f\n", res[0], res[1]);
 
     // Store matrix C
     // _mm256_store_pd(C + Matrix_size * 0, c0);
