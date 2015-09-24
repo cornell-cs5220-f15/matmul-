29855d251b5ebba4e485bc76fcf9cca3a1913502
Bob_MAC <jtcdbob@gmail.com>
2015-09-24 16:03:57 -0400

    print out the middle part to debug_1

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -186,7 +186,7 @@ void mine_fma_dgemm( const double* restrict A, const double* restrict B,
       c3 = _mm256_fmadd_pd(a3, bij, c3); // C = A * B + C;
     }
 
-    res = (double*)&c1;
+    double * res = (double*)&c1;
     printf("td1 elements: %f\t%f\n", res[0], res[1]);
     // Store matrix C
     _mm256_store_pd(C + Matrix_size * 0, c0);
