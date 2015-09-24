4037a22d7e89e1d4ea4ed403a6f5b2af8d24cfd2
Bob_MAC <jtcdbob@gmail.com>
2015-09-24 19:39:24 -0400

    huh...

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -179,14 +179,14 @@ void mine_fma_dgemm( const double* restrict A, const double* restrict B,
       c3  = _mm256_fmadd_pd(a3, bij, c3); // C = A * B + C;
     }
 
-    // double * res = (double*)&c1;
+    double * res = (double*)&c1;
     // printf("td1 elements: %f\t%f\n", res[0], res[1]);
 
     // // Store matrix C
-    _mm256_storeu_pd(&(C + Matrix_size * 0), c0);
-    _mm256_storeu_pd(&(C + Matrix_size * 1), c1);
-    _mm256_storeu_pd(&(C + Matrix_size * 2), c2);
-    _mm256_storeu_pd(&(C + Matrix_size * 3), c3);
+    // _mm256_storeu_pd(C + Matrix_size * 0, c0);
+    // _mm256_storeu_pd(C + Matrix_size * 1, c1);
+    // _mm256_storeu_pd(C + Matrix_size * 2, c2);
+    // _mm256_storeu_pd(C + Matrix_size * 3, c3);
 }
 
 
