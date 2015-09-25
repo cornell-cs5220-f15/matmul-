4ca89e2d9096062b68d15f0e8c5f78cb262b34ab
Bob_MAC <jtcdbob@gmail.com>
2015-09-24 22:04:42 -0400

    minor correction

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -132,7 +132,7 @@ void mine_fma_dgemm( const double* restrict A, const double* restrict B,
     __m256d a2 = _mm256_load_pd(A + Matrix_size * 2);
     __m256d a3 = _mm256_load_pd(A + Matrix_size * 3);
 
-    // scatter_vec(C, a1);
+    scatter_vec(C, a1);
     // int it, jt;
     //
     // printf("============Matrix C inside the loop ============\n");
