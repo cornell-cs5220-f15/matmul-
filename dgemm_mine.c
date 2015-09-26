6390337d80c5c4f870adbfcf48a516e0435008dc
Bob_MAC <jtcdbob@gmail.com>
2015-09-26 19:09:18 -0400

    minor correction

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -210,8 +210,8 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
       for (sbj = 0; sbj < n_inner_blocks; sbj++){
         matrix_copy (M, INNER_BLOCK_SIZE, sbi*INNER_BLOCK_SIZE, sbj*INNER_BLOCK_SIZE, C, C_inner);
         for (sbk = 0; sbk < n_inner_blocks; sbk++){
-          matrix_copy (M, INNER_BLOCK_SIZE, sbi*INNER_BLOCK_SIZE, sbk*INNER_BLOCK_SIZE, A, A_inner);
-          matrix_copy (M, INNER_BLOCK_SIZE, sbk*INNER_BLOCK_SIZE, sbj*INNER_BLOCK_SIZE, B, B_inner);
+          matrix_copy (M, INNER_BLOCK_SIZE, sbk*INNER_BLOCK_SIZE, sbi*INNER_BLOCK_SIZE, A, A_inner);
+          matrix_copy (M, INNER_BLOCK_SIZE, sbj*INNER_BLOCK_SIZE, sbk*INNER_BLOCK_SIZE, B, B_inner);
           mine_fma_dgemm(A_inner, B_inner, C_inner);
           int it, jt;
           printf("Super Inside, Matrix A_inner is:\n");
