5868de70543433b4121deae542e3b2c00240c467
Bob_MAC <jtcdbob@gmail.com>
2015-09-26 18:37:19 -0400

    minor correction

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -202,10 +202,10 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
     int sbi, sbj, sbk;
     for (sbi = 0; sbi < n_inner_blocks; sbi++){
       for (sbj = 0; sbj < n_inner_blocks; sbj++){
-        matrix_copy (BLOCK_SIZE, INNER_BLOCK_SIZE, sbi, sbj, C, C_inner);
+        matrix_copy (M, INNER_BLOCK_SIZE, sbi, sbj, C, C_inner);
         for (sbk = 0; sbk < n_inner_blocks; sbk++){
-          matrix_copy (BLOCK_SIZE, INNER_BLOCK_SIZE, sbi, sbk, A, A_inner);
-          matrix_copy (BLOCK_SIZE, INNER_BLOCK_SIZE, sbk, sbj, B, B_inner);
+          matrix_copy (M, INNER_BLOCK_SIZE, sbi, sbk, A, A_inner);
+          matrix_copy (M, INNER_BLOCK_SIZE, sbk, sbj, B, B_inner);
           mine_fma_dgemm(A_inner, B_inner, C_inner);
           int it, jt;
           printf("Super Inside, Matrix C_inner is:\n");
@@ -216,7 +216,7 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
             printf("\n");
           }
         }
-        matrix_update (BLOCK_SIZE, INNER_BLOCK_SIZE, sbi, sbj, C, C_inner);
+        matrix_update (M, INNER_BLOCK_SIZE, sbi, sbj, C, C_inner);
         // int it, jt;
         // printf("Inside, Matrix C_inner is:\n");
         // for(it = 0; it < M; it ++){
