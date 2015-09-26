edad95c57da5bf307885dd0f1c061a3d0fec9a98
Bob_MAC <jtcdbob@gmail.com>
2015-09-26 18:24:40 -0400

    minor correction

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -207,31 +207,31 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
           int it, jt;
           matrix_copy (BLOCK_SIZE, INNER_BLOCK_SIZE, sbi, sbk, A, A_inner);
           matrix_copy (BLOCK_SIZE, INNER_BLOCK_SIZE, sbk, sbj, B, B_inner);
-          printf("Matrix A_inner is:");
-          for(it = 0; it < M; it ++){
-            for(jt = 0; jt < M; jt ++){
-              printf("%lf \t", A_inner[it*M+jt]);
-            }
-            printf("\n");
+          // printf("Matrix A_inner is:");
+          // for(it = 0; it < M; it ++){
+          //   for(jt = 0; jt < M; jt ++){
+          //     printf("%lf \t", A_inner[it*M+jt]);
+          //   }
+          //   printf("\n");
           }
           mine_fma_dgemm(A_inner, B_inner, C_inner);
         }
-        int it, jt;
-        printf("Matrix C before update is:");
-        for(it = 0; it < M; it ++){
-          for(jt = 0; jt < M; jt ++){
-            printf("%lf \t", C[it*M+jt]);
-          }
-          printf("\n");
-        }
+        // int it, jt;
+        // printf("Matrix C before update is:");
+        // for(it = 0; it < M; it ++){
+        //   for(jt = 0; jt < M; jt ++){
+        //     printf("%lf \t", C[it*M+jt]);
+        //   }
+        //   printf("\n");
+        // }
         matrix_update (BLOCK_SIZE, INNER_BLOCK_SIZE, sbi, sbj, C, C_inner);
-        printf("Matrix C after update is:");
-        for(it = 0; it < M; it ++){
-          for(jt = 0; jt < M; jt ++){
-            printf("%lf \t", C[it*M+jt]);
-          }
-          printf("\n");
-        }
+        // printf("Matrix C after update is:");
+        // for(it = 0; it < M; it ++){
+        //   for(jt = 0; jt < M; jt ++){
+        //     printf("%lf \t", C[it*M+jt]);
+        //   }
+        //   printf("\n");
+        // }
 
       }
     }
