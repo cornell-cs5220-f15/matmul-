c304fba91a7c5af731c44f185e5ea602fd8a994d
Bob_MAC <jtcdbob@gmail.com>
2015-09-26 19:31:55 -0400

    minor correction

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -119,7 +119,8 @@ void mine_fma_dgemm( const double* restrict A, const double* restrict B,
     int i, j;
     for (i = 0; i < Matrix_size; i++){
       // Load one column of C, C(:,i)
-      __m256d c = _mm256_loadu_pd((C + Matrix_size*i));
+      // __m256d c = _mm256_loadu_pd((C + Matrix_size*i));
+      __m256d c; // No need to copy the original for this one
       // Perform FMA on A*B(:,i)
       bij = _mm256_set1_pd(*(B+i*Matrix_size+0));
       c = _mm256_fmadd_pd(a0, bij, c);
