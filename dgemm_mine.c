f5f8c6a91df0bd93a2c9d52d820f20837c26430d
Lim Mingjie, Kenneth <me@kenlimmj.com>
2015-09-29 15:26:15 -0400

    Removed transpose in loop

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -30,6 +30,32 @@ inline void transpose_array(const int M, const double * restrict A, double * res
   }
 }
 
+inline void transpose_4x4(const double * restrict A, double * restrict B)
+{
+  __assume_aligned(A, MEM_BOUNDARY);
+  __assume_aligned(B, MEM_BOUNDARY);
+
+  B[0] = A[0];
+  B[1] = A[4];
+  B[2] = A[8];
+  B[3] = A[12];
+
+  B[4] = A[1];
+  B[5] = A[5];
+  B[6] = A[9];
+  B[7] = A[13];
+
+  B[8] = A[2];
+  B[9] = A[6];
+  B[10] = A[10];
+  B[11] = A[14];
+
+  B[12] = A[3];
+  B[13] = A[7];
+  B[14] = A[11];
+  B[15] = A[15];
+}
+
 /**
  * Multiplies a pair of 4 x 4 matrices using AVX instructions.
  * Requires that all matrix inputs be aligned to 32 bytes or shit hits the fan
@@ -47,7 +73,7 @@ inline void transpose_array(const int M, const double * restrict A, double * res
  * ymm1 ymm4   ymm2 ymm5   ymm3 ymm6   ymm4 ymm7
  *
  * @method dgemm_4x4
- * @param  A         4 x 4 matrix in row-order
+ * @param  A         4 x 4 matrix in column-order
  * @param  B         4 x 4 matrix in column-order
  * @param  C         4 x 4 matrix in column-order
  */
@@ -57,18 +83,27 @@ inline void dgemm_4x4(const double * restrict A, const double * restrict B, doub
   __assume_aligned(B, MEM_BOUNDARY);
   __assume_aligned(C, MEM_BOUNDARY);
 
-  __m256d ymm0, ymm1, ymm2, ymm3, ymm4, ymm5, ymm6, ymm7, ymm9;
+  // Registers for A
+  __m256d ymm0, ymm1, ymm2, ymm3;
 
-  // Convert A to column order
-  // TODO: Switch the input to be in column order so we don't waste time mallocing
-  double *a_transposed = _mm_malloc(4 * 4 * sizeof(double), MEM_BOUNDARY);
-  transpose_array(4, A, a_transposed);
+  // Registers for B
+  __m256d ymm4, ymm5, ymm6, ymm7;
+
+  // Registers for C
+  __m256d ymm8, ymm9, ymm10, ymm11;
+
+  // Load original values of C
+  // These come in front to prevent load-use conflicts later on
+  ymm8 = _mm256_load_pd(C);
+  ymm9 = _mm256_load_pd(C + 4);
+  ymm10 = _mm256_load_pd(C + 8);
+  ymm11 = _mm256_load_pd(C + 12);
 
   // Load columns of A
-  ymm0 = _mm256_load_pd(a_transposed);
-  ymm1 = _mm256_load_pd(a_transposed + 4);
-  ymm2 = _mm256_load_pd(a_transposed + 8);
-  ymm3 = _mm256_load_pd(a_transposed + 12);
+  ymm0 = _mm256_load_pd(A);
+  ymm1 = _mm256_load_pd(A + 4);
+  ymm2 = _mm256_load_pd(A + 8);
+  ymm3 = _mm256_load_pd(A + 12);
 
   // Broadcast B11, B21, B31, and B41
   ymm4 = _mm256_broadcast_sd(B);
@@ -88,8 +123,7 @@ inline void dgemm_4x4(const double * restrict A, const double * restrict B, doub
   ymm4 = _mm256_add_pd(ymm4, ymm6);
 
   // Writeback to memory
-  ymm9 = _mm256_load_pd(C);
-  ymm4 = _mm256_add_pd(ymm4, ymm9);
+  ymm4 = _mm256_add_pd(ymm4, ymm8);
   _mm256_store_pd(C, ymm4);
 
   // Broadcast B12, B22, B32, and B42
@@ -110,7 +144,6 @@ inline void dgemm_4x4(const double * restrict A, const double * restrict B, doub
   ymm4 = _mm256_add_pd(ymm4, ymm6);
 
   // Writeback to memory
-  ymm9 = _mm256_load_pd(C + 4);
   ymm4 = _mm256_add_pd(ymm4, ymm9);
   _mm256_store_pd(C + 4, ymm4);
 
@@ -132,8 +165,7 @@ inline void dgemm_4x4(const double * restrict A, const double * restrict B, doub
   ymm4 = _mm256_add_pd(ymm4, ymm6);
 
   // Writeback to memory
-  ymm9 = _mm256_load_pd(C + 8);
-  ymm4 = _mm256_add_pd(ymm4, ymm9);
+  ymm4 = _mm256_add_pd(ymm4, ymm10);
   _mm256_store_pd(C + 8, ymm4);
 
   // Broadcast B14, B24, B34, and B44
@@ -154,11 +186,8 @@ inline void dgemm_4x4(const double * restrict A, const double * restrict B, doub
   ymm4 = _mm256_add_pd(ymm4, ymm6);
 
   // Writeback to memory
-  ymm9 = _mm256_load_pd(C + 12);
-  ymm4 = _mm256_add_pd(ymm4, ymm9);
+  ymm4 = _mm256_add_pd(ymm4, ymm11);
   _mm256_store_pd(C + 12, ymm4);
-
-  _mm_free(a_transposed);
 }
 
 inline void dgemm_8x8(const double * restrict A, const double * restrict B, double * restrict C)
@@ -167,27 +196,12 @@ inline void dgemm_8x8(const double * restrict A, const double * restrict B, doub
   __assume_aligned(B, MEM_BOUNDARY);
   __assume_aligned(C, MEM_BOUNDARY);
 
-  dgemm_4x4(A,      B,      C);
+  dgemm_4x4(A,      B,      C     );
   dgemm_4x4(A + 32, B,      C + 16);
   dgemm_4x4(A,      B + 32, C + 32);
   dgemm_4x4(A + 32, B + 32, C + 48);
 
   // TODO: Convert the matrix back to correct layout
-}
-
-inline void dgemm_16x16(const double * restrict A, const double * restrict B, double * restrict C)
-{
-
-}
-
-inline void dgemm_32x32(const double * restrict A, const double * restrict B, double * restrict C)
-{
-
-}
-
-inline void dgemm_64x64(const double * restrict A, const double * restrict B, double * restrict C)
-{
-
 }
 
 void square_dgemm(const int M, const double *A, const double *B, double *C)
@@ -235,9 +249,9 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
           {
           	int row_start_inner = ((row_start + row) * M) + k_start;
 
-            for (k = 0; k < k_size; ++k)
+            for (k = 0; k < k_size; k++)
             {
-              a_block[(row * MUL_BLOCK_SIZE) + k] = a_transposed[row_start_inner + k];
+              a_block[(k * MUL_BLOCK_SIZE) + row] = a_transposed[row_start_inner + k];
             }
           }
 
@@ -246,7 +260,7 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
           {
           	int col_start_inner = ((col_start + col)*M) + k_start;
 
-            for (k = 0; k < k_size; ++k)
+            for (k = 0; k < k_size; k++)
             {
               b_block[(col * MUL_BLOCK_SIZE) + k] =  B[col_start_inner + k];
             }
@@ -271,5 +285,4 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
     _mm_free(c_block);
     _mm_free(b_block);
     _mm_free(a_block);
-    _mm_free(a_transposed);
 }
