5a4f6bddec4ba8e041248c7ce3395762f040e15f
rmc298 <rmc298@en-cs-totient-01.coecis.cornell.edu>
2015-09-29 13:54:11 -0400

    Better blocking but not working. Pushing so can edit on my machien.

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -9,7 +9,7 @@ const char* dgemm_desc = "My super awesome dgemm.";
 
 
 #ifndef block_size
-#define block_size ((int) 64)
+#define block_size ((int) 32)
 #endif
 
 #ifndef rect_length
@@ -20,44 +20,46 @@ const char* dgemm_desc = "My super awesome dgemm.";
 void square_dgemm(const int M, const double* restrict A, const double* restrict B, double* restrict C)
 {
   int I,J,K,i,j,k,i_end,j_end,k_end,Aind,Bind,Cind;
-  int B_block_ind, A_block_ind;
+  int C_block_ind, B_block_ind, A_block_ind;
   double BRHS;
   const int n_rect = M/rect_length + (M%rect_length? 1 : 0);
   const int n_blocks = M / block_size + (M%block_size? 1 : 0);
-  double A_block [rect_length*block_size];
-  double B_block [block_size*block_size];
-
-//  double* A_block = (double*) _mm_malloc(rect_length * block_size * sizeof(double),64);
-//  double* B_block = (double*) _mm_malloc(block_size * block_size * sizeof(double),64);
+    
+  double* A_block = (double*) _mm_malloc(rect_length * block_size * sizeof(double),64);
+  double* B_block = (double*) _mm_malloc(block_size * block_size * sizeof(double),64);
+  double* C_block = (double*) _mm_malloc(rect_length*block_size*sizeof(double),64);
   
   // Block loop
   for(J=0; J<n_blocks; ++J) {
     for(K=0; K<n_blocks; ++K) {
       for(I=0; I<n_rect; ++I){
         j_end = ((J+1)*block_size < M? block_size : (M-(J*block_size)));
+        k_end = ((K+1)*block_size < M? block_size : (M-(K*block_size)));
+        i_end = ((I+1)*rect_length < M? rect_length : (M-(I*rect_length)));
         // Perform Copy
         for(j=0;j< j_end; ++j) {
+          C_block_ind = j*rect_length;
           B_block_ind = j*block_size;
+          Cind = (I*rect_length)+(J*block_size+j)*M;
           Bind = (K*block_size)+(J*block_size+j)*M;
-          k_end = ((K+1)*block_size < M? block_size : (M-(K*block_size)));
           for(k=0; k<k_end; ++k) {
             B_block[B_block_ind+k] = B[Bind+k];
             Aind = (I*rect_length)+(K*block_size+k)*M;
             A_block_ind = k*rect_length;
-            i_end = ((I+1)*rect_length < M? rect_length : (M-(I*rect_length)));
             for(i=0; i< i_end; ++i) {
               A_block[A_block_ind+i] = A[Aind+i];
+              C_block[C_block_ind+i] = C[Cind+i];
               }
             }
           }
         //Perform Multiply
           for(j=0;j< j_end; ++j) {
-            Cind = (I*rect_length)+(J*block_size+j)*M;
+            C_block_ind = j*rect_length;
             for(k=0; k<k_end; ++k) {
-              B_block_ind = j*block_size+k;
+              B_block_ind = j*block_size;
               A_block_ind = k*rect_length;
               for(i=0; i< i_end; ++i) {
-                C[Cind+i] += A_block[A_block_ind+i]*B_block[B_block_ind];
+                C_block[C_block_ind+i] += A_block[A_block_ind+i]*B_block[B_block_ind+k];
 //                C[Cind+i+1] += A[A_block_ind+1]*B_block[B_block_ind+1];
 //                C[Cind+i+2] += A[A_block_ind+2]*B_block[B_block_ind+2];
 //                C[Cind+i+3] += A[A_block_ind+3]*B_block[B_block_ind+3];
@@ -68,10 +70,17 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
               }
             }
           }
+          for(j=0;j<j_end;++j) {
+            Cind = (I*rect_length)+(J*block_size+j)*M;
+            C_block_ind = j*rect_length;
+            for(i=0;i<i_end;++i){
+	      C[Cind+i] +=C_block[C_block_ind+i];
+            }
+         }
       }
     }
   }
-  
-//  _mm_free(A_block);
-//  _mm_free(B_block);
+  _mm_free(A_block);
+  _mm_free(B_block);
+  _mm_free(C_block);
 }
