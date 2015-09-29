79800c51f89e893d447b09f7afcf4f3b9cfa550a
Robert Chiodi <rmc298@cornell.edu>
2015-09-29 13:21:02 -0400

    working blocks

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -24,9 +24,11 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
   double BRHS;
   const int n_rect = M/rect_length + (M%rect_length? 1 : 0);
   const int n_blocks = M / block_size + (M%block_size? 1 : 0);
+  double A_block [rect_length*block_size];
+  double B_block [block_size*block_size];
 
-  double* A_block = (double*) _mm_malloc(rect_length * block_size * sizeof(double),64);
-  double* B_block = (double*) _mm_malloc(block_size * block_size * sizeof(double),64);
+//  double* A_block = (double*) _mm_malloc(rect_length * block_size * sizeof(double),64);
+//  double* B_block = (double*) _mm_malloc(block_size * block_size * sizeof(double),64);
   
   // Block loop
   for(J=0; J<n_blocks; ++J) {
@@ -35,13 +37,13 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
         j_end = ((J+1)*block_size < M? block_size : (M-(J*block_size)));
         // Perform Copy
         for(j=0;j< j_end; ++j) {
-          k_end = ((K+1)*block_size < M? block_size : (M-(K*block_size)));
           B_block_ind = j*block_size;
           Bind = (K*block_size)+(J*block_size+j)*M;
+          k_end = ((K+1)*block_size < M? block_size : (M-(K*block_size)));
           for(k=0; k<k_end; ++k) {
+            B_block[B_block_ind+k] = B[Bind+k];
             Aind = (I*rect_length)+(K*block_size+k)*M;
             A_block_ind = k*rect_length;
-            B_block[B_block_ind+k] = B[Bind+k];
             i_end = ((I+1)*rect_length < M? rect_length : (M-(I*rect_length)));
             for(i=0; i< i_end; ++i) {
               A_block[A_block_ind+i] = A[Aind+i];
@@ -51,18 +53,18 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
         //Perform Multiply
           for(j=0;j< j_end; ++j) {
             Cind = (I*rect_length)+(J*block_size+j)*M;
-            B_block_ind = j*block_size;
             for(k=0; k<k_end; ++k) {
+              B_block_ind = j*block_size+k;
               A_block_ind = k*rect_length;
-              for(i=0; i< i_end; i=i+8) {
-                C[Cind+i] += A[A_block_ind+i]*B_block[B_block_ind+k];
-                C[Cind+i+1] += A[A_block_ind+i+1]*B_block[B_block_ind+k+1];
-                C[Cind+i+2] += A[A_block_ind+i+2]*B_block[B_block_ind+k+2];
-                C[Cind+i+3] += A[A_block_ind+i+3]*B_block[B_block_ind+k+3];
-                C[Cind+i+4] += A[A_block_ind+i+4]*B_block[B_block_ind+k+4];
-                C[Cind+i+5] += A[A_block_ind+i+5]*B_block[B_block_ind+k+5];
-                C[Cind+i+6] += A[A_block_ind+i+6]*B_block[B_block_ind+k+6];
-                C[Cind+i+7] += A[A_block_ind+i+7]*B_block[B_block_ind+k+7];
+              for(i=0; i< i_end; ++i) {
+                C[Cind+i] += A_block[A_block_ind+i]*B_block[B_block_ind];
+//                C[Cind+i+1] += A[A_block_ind+1]*B_block[B_block_ind+1];
+//                C[Cind+i+2] += A[A_block_ind+2]*B_block[B_block_ind+2];
+//                C[Cind+i+3] += A[A_block_ind+3]*B_block[B_block_ind+3];
+//                C[Cind+i+4] += A[A_block_ind+4]*B_block[B_block_ind+4];
+//                C[Cind+i+5] += A[A_block_ind+5]*B_block[B_block_ind+5];
+//                C[Cind+i+6] += A[A_block_ind+6]*B_block[B_block_ind+6];
+//                C[Cind+i+7] += A[A_block_ind+7]*B_block[B_block_ind+7];
               }
             }
           }
@@ -70,6 +72,6 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
     }
   }
   
-  _mm_free(A_block);
-  _mm_free(B_block);
+//  _mm_free(A_block);
+//  _mm_free(B_block);
 }
