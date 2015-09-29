7bcde1bbaa4ceac58efaac00d4bda44de9115ce0
rmc298 <rmc298@en-cs-totient-01.coecis.cornell.edu>
2015-09-29 17:25:41 -0400

    This is most likely the final product. Runs faster not on node 2, changed rect_length, reduced Flop/s and can run with quick parallel switch.

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,11 +1,11 @@
 const char* dgemm_desc = "My super awesome dgemm.";
 
 #ifndef block_size
-#define block_size ((int) 64)
+#define block_size ((int) 32)
 #endif
 
 #ifndef rect_length
-#define rect_length ((int) 256)
+#define rect_length ((int) 512)
 #endif
 
 // Blocked type
@@ -19,6 +19,7 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
   const int n_blocks = M / block_size + (M%block_size? 1 : 0);
   
   //Block Loop
+  #pragma parallel
   for(J=0; J<n_blocks; ++J) {
     for(K=0; K<n_blocks; ++K) {
       for(I=0; I<n_rect; ++I){
